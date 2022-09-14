d96d9be6f3494649b267e3c08152b12cbfbddb78
Sean Outram <outramsean0@gmail.com>
2022-09-14 09:38:03 +0100

    Reorganise

--- DiceGame/Form1.cs
+++ DiceGame/Form1.cs
@@ -12,6 +12,16 @@ namespace DiceGame
             int num = (random.Next(1, Maxinum) + 1);
             textBox.Text = num.ToString();
         }
+        private void DiceButton2_Click(object sender, EventArgs e)
+        {
+            // 10 Dice
+            generate(DicetextBox2, 10);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 6 Dice
+            generate(DiceTextBox1, 6);
+        }
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
@@ -21,13 +31,6 @@ namespace DiceGame
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // 6 Dice
-            generate(DiceTextBox1, 6);
-        }
-
         private void Dice2Box_Enter(object sender, EventArgs e)
         {
 
@@ -36,12 +39,6 @@ namespace DiceGame
         private void DicetextBox2_TextChanged(object sender, EventArgs e)
         {
 
-        }
-
-        private void DiceButton2_Click(object sender, EventArgs e)
-        {
-            // 10 Dice
-            generate(DicetextBox2, 10);
         }
     }
 }
